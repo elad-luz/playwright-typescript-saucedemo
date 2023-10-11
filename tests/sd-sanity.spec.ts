eb28f5d5e385e1797d89637c0588d8844281a6ce
elad-luz <elad.luz@gmail.com>
2023-10-12 01:02:43 +0300

    update first saucedemo test

--- tests/sd-sanity.spec.ts
+++ tests/sd-sanity.spec.ts
@@ -6,17 +6,14 @@ test('test', async ({ page }) => {
  // Expect LI page title "to contain" a substring
     await expect(page).toHaveTitle(/Swag Labs/);
 
-  // input User & Pass strings to TextBoxs
-  await page.getByText('Accepted usernames are:standard_userlocked_out_userproblem_userperformance_glitch_userPassword for all users:secret_sauce');
-
-  // input User & Pass strings to TextBoxs
+  // Successful Login using proper inputs of User & Pass into TextBoxs
   await page.locator('[data-test="username"]').click();
   await page.locator('[data-test="username"]').fill('standard_user');
   await page.locator('[data-test="password"]').click();
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
 
-  // Add Products
+  // Add 2 Products to cart - and click the cart to see they were added
   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
   await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
   await page.locator('a').filter({hasText: '2'}).click();
